62ec36076d8cc73af100a9c93a62c830896fc739
Rebecca <rebecca.frailich@gmail.com>
2016-01-16 15:20:12 -0500

    playlist object started

---
+++ LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/Playlist.java
@@ -0,0 +1,26 @@
+package com.example.giraffe.lifesountrack;
+
+/**
+ * Created by Giraffe on 2016-01-16.
+ */
+public class Playlist {
+
+    /** The name of this Playlist */
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addSong(String path, Integer bpm){
+
+    }
+
+    public void removeSong(String path){
+
+    }
+}
