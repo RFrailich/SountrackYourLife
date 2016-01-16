02902b403b99996e30081439b7e5be3d7ccdc56e
Rebecca <rebecca.frailich@gmail.com>
2016-01-16 17:46:52 -0500

    starting calendar

--- LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/DBHelper.java
+++ LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/DBHelper.java
@@ -3,5 +3,5 @@ package com.example.giraffe.lifesountrack;
 /**
  * Created by Giraffe on 2016-01-16.
  */
-public class Song {
+public class DBHelper {
 }
---
+++ LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/Week.java
@@ -0,0 +1,7 @@
+package com.example.giraffe.lifesountrack;
+
+/**
+ * Created by Giraffe on 2016-01-16.
+ */
+public class Week {
+}
