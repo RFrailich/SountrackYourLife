595a95a15f44b8550f0af801f62f14cb50e49417
jpaprakis <j.paprakis@mail.utoronto.ca>
2016-01-17 03:08:02 -0500

    kfrf

--- LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/MainActivity.java
+++ LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    static int wat;
    // private int n;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button mPlayButton = (Button) findViewById(R.id.playMain);
+        Button myMenu = (Button) findViewById(R.id.list);
         // Default button, if need set it in xml via background="@drawable/default"
         mPlayButton.setBackgroundResource(R.drawable.play);
         mPlayButton.setOnClickListener(mTogglePlayButton);
+        myMenu.setOnClickListener(mToggleMenuButton);
     }
 
     View.OnClickListener mTogglePlayButton = new View.OnClickListener(){
@@ -41,6 +44,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
+
+    View.OnClickListener mToggleMenuButton = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            setContentView(R.layout.activity_menu);
+            Button lilB = (Button) findViewById(R.id.lowB);
+            Button midB = (Button) findViewById(R.id.midB);
+            Button bigB = (Button) findViewById(R.id.highB);
+            //setContentView(R.layout.activity_menu);
+        }
+    };
+
+    public void mToggleLoSong() {
+        wat = 0;
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mToggleMidSong() {
+        wat = 1;
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mToggleHiSong() {
+        wat = 2;
+        setContentView(R.layout.activity_main);
+    }
 
     public void list(View view){
         Intent nextPage = new Intent(this, NewPlaylistActivity.class);
