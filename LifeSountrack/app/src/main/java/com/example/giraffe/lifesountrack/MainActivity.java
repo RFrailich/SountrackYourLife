2dd6387e2aac61b128e6a4b09fdcb5d3669ee24e
jpaprakis <j.paprakis@mail.utoronto.ca>
2016-01-17 01:43:59 -0500

    added changes

--- LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/MainActivity.java
+++ LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.giraffe.lifesountrack;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mediaPlayer;
+   // private int n;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //n = 0;
+        //Code to choose a song with a given BPM, right now choose a specific song
+        mediaPlayer = MediaPlayer.create(this, R.raw.slow); //change to logical  choice later
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button mPlayButton = (Button) findViewById(R.id.playMain);
@@ -20,13 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
     View.OnClickListener mTogglePlayButton = new View.OnClickListener(){
         boolean clicked = false;
+
         @Override
         public void onClick(View v){
             // change your button background
-
             if(clicked){
+                mediaPlayer.pause();
                 v.setBackgroundResource(R.drawable.play);
             }else{
+                mediaPlayer.start();
                 v.setBackgroundResource(R.drawable.pause);
             }
 
