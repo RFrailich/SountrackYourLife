2c6ba3b1fef976fd79f8c4e982a628bae682992e
mosake <kellysammo@gmail.com>
2016-01-16 20:36:07 -0500

    yay

--- LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/MainActivity.java
+++ LifeSountrack/app/src/main/java/com/example/giraffe/lifesountrack/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,20 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mPlayButton = (Button) findViewById(R.id.playMain);
+        // Default button, if need set it in xml via background="@drawable/default"
+        mPlayButton.setBackgroundResource(R.drawable.play);
+        mPlayButton.setOnClickListener(mTogglePlayButton);
     }
 
-    public void play(View view) {
-        Intent nextPage = new Intent(this,PauseActivity.class);
-        startActivity(nextPage);
-    }
+    View.OnClickListener mTogglePlayButton = new View.OnClickListener(){
+        boolean clicked = false;
+        @Override
+        public void onClick(View v){
+            // change your button background
+
+            if(clicked){
+                v.setBackgroundResource(R.drawable.play);
+            }else{
+                v.setBackgroundResource(R.drawable.pause);
+            }
+
+            clicked = !clicked; // reverse
+        }
+
+    };
 
     public void settings(View view){
-        Intent nextPage = new Intent(this, PlayActivity.class);
+        Intent nextPage = new Intent(this, SettingsActivity.class);
         startActivity(nextPage);
     }
 
     public void list(View view){
-        Intent nextPage = new Intent(this, PlayActivity.class);
+        Intent nextPage = new Intent(this, NewPlaylistActivity.class);
         startActivity(nextPage);
     }
 }
